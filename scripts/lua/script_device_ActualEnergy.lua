4cfee5a47102c4f5a4df8175156a3a141b78fc94
mrhedstrom <mrhedstrom@gmail.com>
2016-12-02 15:53:50 +0100

    Updated script to work without user variables due to new handling of uservariables in new versions of domoticz.

--- scripts/lua/script_device_ActualEnergy.lua
+++ scripts/lua/script_device_ActualEnergy.lua
@@ -1,30 +1,31 @@
 --Domoticz Script to get medium actual energy between energy counter readings and puts it on a dummy device
 
---First create a dummy energy meter and two user variables as numbers.
+--First create a dummy energy meter.
 --Then enter the details below
 
 --Name of the real energy counter
 energyCounter = 'Elförbrukning'
+--Name of the created dummy energy meter with the new actual value
+dummyEnergyMeter = 'Elförbrukning3'
 --ID of the created dummy energy meter with the new actual value
-dummyEnergyMeterid = 126
-
---The names of two user variables to keep track of previous value due to wrong values from dummy in otherdevices_lastupdate and otherdevices_svalues
-userVariableTimestamp = 'LastEnergyTimestamp'
-userVariableLastCount = 'LastEnergyCount'
+dummyEnergyMeterid = 378
 
 commandArray = {}
 if devicechanged[energyCounter] then
 	--calculate new actual value
-	actual = ((tonumber(otherdevices_svalues[energyCounter]) - tonumber(uservariables[userVariableLastCount])))/((os.time()-uservariables[userVariableTimestamp])/3600)
+	s = otherdevices_lastupdate[dummyEnergyMeter]
+	if s == nil then
+		print('First  time script is ever triggered. Update only counter. Actual value will be updated next time.')
+		actual = 0
+	else
+		t = os.time{year=string.sub(s, 1, 4), month=string.sub(s, 6, 7), day=string.sub(s, 9, 10), hour=string.sub(s, 12, 13), min=string.sub(s, 15, 16), sec=string.sub(s, 18, 19)}
+		actual = (tonumber(otherdevices_svalues[energyCounter]) - tonumber(string.match(otherdevices_svalues[dummyEnergyMeter], "%d+%.*%d*;(%d+%.*%d*)")))/(os.difftime(os.time(), t)/3600)
+	end
 	
 	--update dummy energy meter
 	commandArray[1] = {['UpdateDevice'] = dummyEnergyMeterid .. "|0|" .. actual .. ";" .. otherdevices_svalues[energyCounter]}
 	
-	--update user variables
-	commandArray[2] = {['Variable:'..userVariableTimestamp] = tostring(os.time())}
-	commandArray[3] = {['Variable:'..userVariableLastCount] = otherdevices_svalues[energyCounter]}
-	
-	print("DummyEnergy: " .. actual .. " W, " .. otherdevices_svalues[energyCounter] .. " kWh")
+	print(dummyEnergyMeter .. ": " .. actual .. " W, " .. otherdevices_svalues[energyCounter] .. " kWh")
 end
 
 return commandArray
